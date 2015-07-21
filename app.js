3bb839476744c518169a997f60f6ed8822c6b860
Josh Hamblin <joshhamblin9@gmail.com>
2015-07-20 22:20:02 -0600

    finished project

---
+++ app.js
@@ -0,0 +1,61 @@
+var color = 'white'
+var colors = 'white green red blue yellow'
+
+$(document).ready(function() {
+
+$('.box').on('click', function(){
+	$(this).addClass('white');
+})
+
+$('.box').on('dblclick', function(){
+	$(this).removeClass('white');
+})
+
+$('#reset').on('click', function(){
+	$('.box').removeClass('white')
+})
+
+
+$('#red').on('click', function() {
+	color = 'red';
+})
+
+$('#blue').on('click', function () {
+	color = 'blue';
+})
+
+$('#green').on('click', function() {
+	color = 'green';
+})
+
+$('#yellow').on('click', function() {
+	color = 'yellow';
+})
+
+$('#white').on('click', function() {
+	color = 'white';
+})
+
+$('.box').on('click', function() {
+	$(this).addClass(color);
+})
+
+$('.box').on('dblclick', function() {
+	$(this).removeClass(color);
+})
+
+$('#reset').on('click', function() {
+	$('.box').removeClass(color);
+})
+
+$('.box').on('dblclick', function () {
+	$(this).removeClass(colors);
+})
+
+$('#reset').on('click', function() {
+	$('.box').removeClass(colors);
+})
+
+});
+
+
